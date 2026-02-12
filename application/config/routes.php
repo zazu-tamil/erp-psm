49257f4ff7049876b571a3fcc645b1f42fb84e43
Naveen M <naveenmanogaren@gmail.com>
2026-02-12 20:30:59 +0530

    update

--- application/config/routes.php
+++ application/config/routes.php
@@ -261,3 +261,5 @@ $route['addt-charges-type-list/(:num)'] = 'master/addt_charges_type_list/$1';
 $route['tender-enquiry-timeline'] = 'tender/tender_enquiry_timeline';
 $route['tender-enquiry-timeline/(:num)'] = 'tender/tender_enquiry_timeline/$1';
 
+//summary report
+$route['tender-enquiry-summary-report'] = 'Summary/tender_enquiry_summary_report'; 
